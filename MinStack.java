c6fb5bb2e8dc14cf49839951bc4608f6359583b8
shreeraam-sys <shreeraamr@gmail.com>
2024-05-14 11:28:26 -0400

    Code implementation for min stack with single stack

--- MinStack.java
+++ MinStack.java
@@ -5,7 +5,8 @@ import java.util.Stack;
 
 
 /**
- * Using 2 stacks storing the corresponding min value for each stack entry in the minStack
+ * Using single stack storing the corresponding min value if the value to be pushed is smaller than the min
+ *
  *
  * Time Complexity: O(1) (push, pop, peek, top, getMin)
  * Space Complexity: O(n)
@@ -13,25 +14,35 @@ import java.util.Stack;
 class MinStack {
 
     Stack<Integer> stack;
-    Stack<Integer> minStack;
+    int minVal;
 
     public MinStack() {
-
+        minVal = Integer.MAX_VALUE;
         stack = new Stack<>();
-        minStack = new Stack<>();
     }
 
+    /*
+     *push the min value only when the current vale is equal or mininum
+     */
     public void push(int val) {
 
+        if(val<=minVal) {
+            stack.push(minVal);
+            minVal = val;
+        }
         stack.push(val);
-        minStack.push(minStack.empty()? val:Math.min(val, minStack.peek()));
+
     }
 
+    /*
+    if the popped value is equal to the min value, the pointer is in the original place of the min      element, pop second time to get the min value for the value present in the stack and consider it as min
+     */
     public void pop() {
 
-
-        stack.pop();
-        minStack.pop();
+        int poppedVal = stack.pop();
+        if(poppedVal == minVal) {
+            minVal = stack.pop();
+        }
 
     }
 
@@ -40,10 +51,18 @@ class MinStack {
     }
 
     public int getMin() {
-        return minStack.peek();
+        return minVal;
     }
 }
 
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
 /**
  * Your MinStack object will be instantiated and called as such:
  * MinStack obj = new MinStack();
