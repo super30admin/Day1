3d4d136b8694cdde1cd98dce5b8656d40a88e1b6
gsprasannabalaji <govindarajalusaras.p@northeastern.edu>
2024-05-14 08:51:10 -0400

    Implemented the solution for design 1

--- Sample.java
+++ Sample.java
@@ -5,3 +5,31 @@
 
 
 // Your code here along with comments explaining your approach
+
+/*
+    TC - O(1)
+    SC - O(10^6 + 1) ~ O(1)
+    Approach - I have used the boolean array with the maximum size mentioned in the problem
+    and using the index as the key/element for the manipulation of add, remove and contains to return the result.
+*/
+
+class MyHashSet {
+
+    boolean arr[];
+
+    public MyHashSet() {
+        arr = new boolean[1000000 + 1];
+    }
+
+    public void add(int key) {
+        arr[key] = true;
+    }
+
+    public void remove(int key) {
+        arr[key] = false;
+    }
+
+    public boolean contains(int key) {
+        return arr[key];
+    }
+}
