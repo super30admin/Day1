4de7ad947afbd094d88f509bf76025af73c95e21
sohams24 <sohamsatam537@gmail.com>
2024-05-14 20:21:24 -0700

    problem 1 complete

---
+++ problem1.py
@@ -0,0 +1,80 @@
+'''
+Time complexity:
+add: O(1)
+remove: O(1)
+contains: O(1)
+
+'''
+
+class MyHashSet:
+
+  def __init__(self):
+    self.numberOfBuckets = 1000
+    self.bucketItems = 1000
+    self.buckets = [None] * self.numberOfBuckets
+
+  def getBucket(self, key):
+    return key % self.numberOfBuckets
+
+  def getBucketItem(self, key):
+    return key // self.numberOfBuckets
+
+  def add(self, key) -> None:
+    bucket = self.getBucket(key)
+    bucketItem = self.getBucketItem(key)
+    if self.buckets[bucket] == None:
+      if bucket == 0:
+        self.buckets[bucket] = [False] * (self.bucketItems + 1)
+      else:
+        self.buckets[bucket] = [False] * self.bucketItems
+    self.buckets[bucket][bucketItem] = True
+
+  def remove(self, key) -> None:
+    bucket = self.getBucket(key)
+    if self.buckets[bucket] != None:
+      bucketItem = self.getBucketItem(key)
+      self.buckets[bucket][bucketItem] = False
+
+  def contains(self, key) -> bool:
+    bucket = self.getBucket(key)
+    if self.buckets[bucket] == None:
+      return False
+    bucketItem = self.getBucketItem(key)
+    return self.buckets[bucket][bucketItem]
+
+
+# Your MyHashSet object will be instantiated and called as such:
+obj = MyHashSet()
+print('Add 52')
+obj.add(52)
+print('Add 45')
+obj.add(45)
+print('Add 545')
+obj.add(545)
+print('Add 6577')
+obj.add(6577)
+print('Contains 45:', obj.contains(45))
+print('Contains 354:', obj.contains(354))
+print('Remove 45')
+obj.remove(45)
+print('Add 354')
+obj.add(354)
+print('Contains 45:', obj.contains(45))
+print('Contains 354:', obj.contains(354))
+
+print('Contains 1000000:', obj.contains(1000000))
+print('Contains 999999:', obj.contains(999999))
+print('Add 65465')
+obj.add(65465)
+print('Add 245601')
+obj.add(245601)
+print('Add 1000000')
+obj.add(1000000)
+print('Add 999999')
+obj.add(999999)
+print('Contains 1000000:', obj.contains(1000000))
+print('Contains 999999:', obj.contains(999999))
+print('Contains 245601:', obj.contains(245601))
+print('Remove 999999')
+obj.remove(999999)
+print('Contains 999999:', obj.contains(999999))
