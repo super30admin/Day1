deda7dfba98e70cdd802e31620045a5e476149a3
Supreeta Anand Byatnal <38351523+supreeta-anand-byatnal@users.noreply.github.com>
2024-06-18 18:20:50 -0400

    Completed Design-1

---
+++ MinStack.java
@@ -0,0 +1,32 @@
+
+//2. 
+// Time Complexity : O(1)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+import java.util.*;
+
+class MinStack {
+    int min = Integer.MAX_VALUE;
+    Stack<Integer> stack = new Stack<Integer>();
+    public void push(int x) {
+           if(x <= min){          
+            stack.push(min);
+            min=x;
+        }
+        stack.push(x);
+    }
+
+    public void pop() {
+         if(stack.pop() == min) min=stack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return min;
+    }
+}
