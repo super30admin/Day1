6c21a1ca7779c8383ef022dcfe7f9545018df2db
mihirjethwa97 <mihirjethwa97@gmail.com>
2024-06-18 13:58:44 -0400

    Problem 2 updated

--- MinStack.java
+++ MinStack.java
@@ -1,45 +1,44 @@
 class MinStack {
-    public:
-    std::vector<int> stack;
-    std::vector<int> minValue;
-    MinStack() {
+
+    ArrayList<Integer> values = new ArrayList<>();
+    ArrayList<Integer> minValues = new ArrayList<>();
+
+    public MinStack() {
 
     }
 
-    void push(int val) {
-        if(stack.size()==0){
-            stack.push_back(val);
-            minValue.push_back(val);
+    public void push(int val) {
+        if(values.size()==0){
+            values.add(val);
+            minValues.add(val);
         }else{
-            int min = minValue.back();
-            stack.push_back(val);
-            if(std::min(val, min) == val){
-                minValue.push_back(val);
-            }else{
-                minValue.push_back(min);
-            }
+            int lastValue = minValues.get(minValues.size()-1);
+            int min = Math.min(lastValue, val);
+            values.add(val);
+            minValues.add(min);
         }
+
     }
 
-    void pop() {
-        minValue.pop_back();
-        stack.pop_back();
+    public void pop() {
+        values.remove(values.size()-1);
+        minValues.remove(minValues.size()-1);
     }
 
-    int top() {
-        return stack.back();
+    public int top() {
+        return values.get(values.size()-1);
     }
 
-    int getMin() {
-        return minValue.back();
+    public int getMin() {
+        return minValues.get(minValues.size()-1);
     }
-};
+}
 
 /**
  * Your MinStack object will be instantiated and called as such:
- * MinStack* obj = new MinStack();
- * obj->push(val);
- * obj->pop();
- * int param_3 = obj->top();
- * int param_4 = obj->getMin();
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
  */
