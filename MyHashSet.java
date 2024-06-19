4f0f85388891509c2e98d80e9d179ca90e23221c
Nithin Kumar Pamidimukkala <nik@Nithins-MacBook-Pro.local>
2024-06-19 14:25:46 -0700

    Updated the technique to use double hashing

--- MyHashSet.java
+++ MyHashSet.java
@@ -1,32 +1,60 @@
 /*
  Time Complexity : O(1)
- Space Complexity : O(N) where N = 1000001
+ Space Complexity : O(N) worst case N = 100001
  Did this code successfully run on Leetcode : Yes
  Any problem you faced while coding this : Help needed in
  for the problem and then implemented the solution after thorough understanding
 
- Used an array data structure of size 1000001(which can accommodate all the integer keys)
- to design the hashset where the index is the key and the bool value present in those indices
- represent if a key is present. If the value is true, a key is present and if it is false, it is not present.
+ Implemented the double hashing technique discussed in class. First, declare an outer array of size 1000
+ and only when a key is to be inserted into a specific index, we create another static array of size 1000
+ to add the elements into it.
  */
 
 public class MyHashSet {
-    boolean[] data;
+    boolean[][] data;
+    int outerArraySize, innerArraySize;
+
     public MyHashSet() {
-        data = new boolean[1000001];
+        this.outerArraySize = 1000;
+        this.innerArraySize = 1000;
+        this.data = new boolean[outerArraySize][];
+    }
+
+    private int outerIndexHashFunction(int key) {
+        return key%1000;
+    }
+
+    private int innerIndexHashFunction(int key) {
+        return key/1000;
     }
 
     public void add(int key) {
-        data[key] = true;
+        int outerIndex = outerIndexHashFunction(key);
+        int innerIndex = innerIndexHashFunction(key);
+        if(data[outerIndex] == null) {
+            if(outerIndex == 0) {
+                data[outerIndex] = new boolean[innerArraySize+1];
+            }
+            else {
+                data[outerIndex] = new boolean[outerArraySize];
+            }
+        }
+        data[outerIndex][innerIndex] = true;
     }
 
     public void remove(int key) {
-        data[key] = false;
+        int outerIndex = outerIndexHashFunction(key);
+        int innerIndex = innerIndexHashFunction(key);
+        if(data[outerIndex] == null) return;
+        data[outerIndex][innerIndex] = false;
     }
 
     public boolean contains(int key) {
-        System.out.println(data[key]);
-        return data[key];
+        int outerIndex = outerIndexHashFunction(key);
+        int innerIndex = innerIndexHashFunction(key);
+        if(data[outerIndex] == null) return false;
+        System.out.println("The key is in data: " + data[outerIndex][innerIndex]);
+        return data[outerIndex][innerIndex];
     }
 
     public static void main(String[] args) {
