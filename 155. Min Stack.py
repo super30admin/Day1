d5794b4296dac19f190fa0ab0f180a1d0c35df61
poojagiri009 <giri.r.pooja@gmail.com>
2024-06-27 11:42:16 -0400

    Design-1 completed

---
+++ 155. Min Stack.py
@@ -0,0 +1,74 @@
+class MinStack:
+
+    #time comp = O(1) , space comp = O(n)
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+        self.Min = int(sys.maxsize)
+        self.minStack.append(self.Min)
+
+    def push(self, val: int) -> None:
+        if val <= self.Min:
+            self.Min = val
+        self.stack.append(val) #push val in stack
+        self.minStack.append(self.Min) #push min value in min stack
+
+        
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
+        self.Min = self.minStack[-1]
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+        
+    def getMin(self) -> int:
+        return self.Min
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
+
+
+
+
+#--------------Another way using only 1 stack-----------------
+class MinStack:
+
+    #all operations time comp = O(1) , space comp = O(n) but could be less that first approach
+    def __init__(self):
+        self.stack = []
+        self.Min = int(sys.maxsize)
+
+    def push(self, val: int) -> None:
+        if val <= self.Min:
+            self.stack.append(self.Min)
+            self.Min = val
+        self.stack.append(val) #push val in stack
+
+
+    def pop(self) -> None:
+        popped = self.stack.pop()
+        if popped == self.Min:
+            self.Min = self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+        
+    def getMin(self) -> int:
+        return self.Min
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
---
+++ 705. Design HashSet.py
@@ -0,0 +1,45 @@
+class MyHashSet:
+
+    def __init__(self):
+        self.buckets = 1000
+        self.bucketItems = 1000
+        self.storage = [None] * self.buckets
+    
+    def getBucket(self,key):
+        return key % self.buckets
+
+    def getBucketItem(self,key):
+        return key // self.buckets
+
+    # works in TC O(1)    
+    def add(self, key: int) -> None:
+        bucket = self.getBucket(key)
+        bucketItem = self.getBucketItem(key)
+        if self.storage[bucket] == None:
+            if bucket == 0:
+                self.storage[bucket] = [None] * (self.buckets + 1) # 0 to 1000 -> if key is 1000000
+            else:
+                self.storage[bucket] = [None] * self.buckets  # 0 to 999
+        self.storage[bucket][bucketItem] = True
+        
+    #works in TC O(1)    
+    def remove(self, key: int) -> None:
+        bucket = self.getBucket(key)
+        bucketItem = self.getBucketItem(key)
+        if self.storage[bucket] == None:
+            return
+        self.storage[bucket][bucketItem] = False
+
+    # works in TC O(1)    
+    def contains(self, key: int) -> bool:
+        bucket = self.getBucket(key)
+        bucketItem = self.getBucketItem(key)
+        if self.storage[bucket] == None:
+            return False
+        return self.storage[bucket][bucketItem] == True
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
