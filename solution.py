b7d6108d5d2fb0a141463978766cf1fe4cb68304
Nehayj100 <nehayj100@tamu.edu>
2024-07-24 16:29:11 -0500

    modified to O(1) solution

--- solution.py
+++ solution.py
@@ -1,57 +1,36 @@
 class MyHashSet(object):
-    class Node:
-        def __init__(self, key):
-            self.key = key
-            self.next = None
-
     def __init__(self):
-        print("in init")
-        self.head = None
-        self.tail = self.head
+        self.buckets = 1000
+        self.bucketItems = 1000
+        self.storage = [-1]*1001
+
+    def getBuckets(self, key):
+        return key / self.bucketItems
+
+    def getBucketItems(self, key):
+        return key % self.buckets
 
     def add(self, key):
-        new_node = self.Node(key)
-        if self.head == None:
-            print("creating head", key)
-            self.head = new_node
-            self.tail = self.head
-        else:
-            print("creating next node: ", key)
-            self.tail.next = new_node
-            self.tail = new_node
+        bucket = self.getBuckets(key)
+        bucketItem = self.getBucketItems(key)
+        if self.storage[bucket] == -1:
+            self.storage[bucket] = [-1]*1000
+        self.storage[bucket][bucketItem] = key
 
     def remove(self, key):
-        prev, curr = None, self.head
-        if self.head is None:
-            return
-        if self.head and self.tail and key == self.tail.key and key == self.head.key:
-            self.head = None
-            self.tail = None
-            return
-        if key == self.head.key:
-            self.head = self.head.next
-            return
-        if key == self.tail.key:
-            itr = self.head
-            while itr.next is not self.tail:
-                itr = itr.next
-            itr.next = None
-        while curr is not self.tail:
-            if curr.key == key:
-                prev.next = curr.next
-                curr.next = None
-                return
-            prev = curr
-            curr = curr.next
+        bucket = self.getBuckets(key)
+        bucketItem = self.getBucketItems(key)
+        if self.contains(key):
+            self.storage[bucket][bucketItem] = -1
 
 
     def contains(self, key):
-        itr = self.head
-        while itr is not None:
-            if itr.key == key:
-                return True
-            itr = itr.next
-        return False
+        bucket = self.getBuckets(key)
+        bucketItem = self.getBucketItems(key)
+        if self.storage[bucket] != -1 and self.storage[bucket][bucketItem] == key:
+            return True
+        else:
+            return False
 
 
 # Your MyHashSet object will be instantiated and called as such:
