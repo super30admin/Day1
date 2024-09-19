3a485c95d7577a150a42482a04b623d0100a9ff7
Bhagyashri <33651055+Btushir@users.noreply.github.com>
2024-09-19 15:18:09 -0700

    Complete desig min stack and hash set

--- Sample.java
+++ Sample.java
@@ -7,3 +7,10 @@
 // This means that modifying one of the sublists will affect all the others because they all refer to the same object in memory.
 // Thus use self.arrayBucket = [[] for _ in range(1000)]
 // Your code here along with comments explaining your approach
+
+// Both list and deque have O(1) time complexity for push, pop, and peek operations when used as stacks.
+// Lists have some overhead due to occasional resizing when the list exceeds its capacity
+//  (though this happens in amortized O(1) time, not per operation).
+// deque is designed for efficient appending and popping from both ends, so if you need
+// a stack with more flexibility or want to avoid the dynamic resizing of lists, deque is slightly better in terms
+// of performance stability.
