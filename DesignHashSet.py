c5fef9382fc101cbfd880d40f2c5c68f01582cf0
SriVinayA <srivinay.appari@gmail.com>
2024-09-19 15:56:05 -0700

    hashset

---
+++ DesignHashSet.py
@@ -0,0 +1,74 @@
+class ListNode:
+    def __init__(self, key):
+        self.key = key
+        self.next = None
+
+class MyHashSet:
+
+    def __init__(self):
+        self.size = 10000
+        self.buckets = [None] * self.size #[None for _ in range(10000)]
+    
+    def _hash(self, key:int) -> int:
+        return key % self.size
+
+    def add(self, key: int) -> None:
+        index = self._hash(key)
+
+        #if bucket is null
+        if not self.buckets[index]:
+            self.buckets[index] = ListNode(key)
+        #if bucket is not null
+        else:
+            curr = self.buckets[index]
+            #traverse
+            while curr:
+                #if its first node
+                if curr.key == key:
+                    return
+                #if next node is null
+                if not curr.next:
+                    curr.next = ListNode(key)
+                curr = curr.next
+
+    def remove(self, key: int) -> None:
+        index = self._hash(key)
+        
+        # If the bucket is empty, return
+        if not self.buckets[index]:
+            return
+
+        curr = self.buckets[index]
+
+        # If the first node is the one to remove
+        if curr.key == key:
+            self.buckets[index] = curr.next
+            return
+
+        # Traverse the list to find the node to remove
+        while curr.next:
+            if curr.next.key == key:
+                curr.next = curr.next.next
+                return
+            curr = curr.next
+
+    def contains(self, key: int) -> bool:
+        index = self._hash(key)
+
+        #if bucket is null
+        if not self.buckets[index]:
+            return False
+        
+        curr = self.buckets[index]
+        while curr:
+            if curr.key == key:
+                return True
+            curr = curr.next
+        return False
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
