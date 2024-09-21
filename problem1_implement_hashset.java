1181d57f098e81f4f234038685e85695ad5a9b95
Sai Manoj Gaddipati <30746079+sai-manoj-gaddipati@users.noreply.github.com>
2024-09-20 18:19:06 -0700

    Update problem1_implement_hashset.java

--- problem1_implement_hashset.java
+++ problem1_implement_hashset.java
@@ -5,3 +5,63 @@
 
 
 // Your code here along with comments explaining your approach
+
+class MyHashSet {
+    int primaryBuckets;
+    int secondaryBuckets;
+    boolean[][] storage;
+
+    public MyHashSet() {
+        this.primaryBuckets = 1000;
+        this.secondaryBuckets = 1000;
+        this.storage = new boolean[primaryBuckets][];
+    }
+
+    private int getPrimaryHash(int key){
+        return key % primaryBuckets;
+    }
+
+    private int getSecondaryHash(int key){
+        return key / secondaryBuckets;
+    }
+    
+    public void add(int key) {
+        int primaryHash = getPrimaryHash(key);
+        if(storage[primaryHash] == null){
+            if(primaryHash == 0){
+                storage[primaryHash] = new boolean[secondaryBuckets+1];//edge case handling
+            }
+            else{
+                storage[primaryHash] = new boolean[secondaryBuckets];
+            }
+            
+        }
+        int secondaryHash = getSecondaryHash(key);
+        storage[primaryHash][secondaryHash] = true;
+    }
+    
+    public void remove(int key) {
+        int primaryHash = getPrimaryHash(key);
+        int secondaryHash = getSecondaryHash(key);
+        if(storage[primaryHash] != null){
+            storage[primaryHash][secondaryHash] = false;
+            // return;
+        }
+        else{
+            return;
+        }
+        
+    }
+    
+    public boolean contains(int key) {
+        int primaryHash = getPrimaryHash(key);
+        int secondaryHash = getSecondaryHash(key);
+        if(storage[primaryHash] == null){
+            return false;
+        }
+        else{
+            return storage[primaryHash][secondaryHash]; //no need to check bcz default is false
+        }
+        
+    }
+}
