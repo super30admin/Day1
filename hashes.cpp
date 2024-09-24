b80db3418e11fcef099c3b9a7c23fed7a2c7f26b
Aravindan Kalaiyarasu <aravindankalaiyarasu@gmail.com>
2024-09-23 17:42:04 -0700

    Updated hashes.cpp

--- hashes.cpp
+++ hashes.cpp
@@ -1,3 +1,8 @@
+// Time Complexity : O(1)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes, 54 ms
+// Any problem you faced while coding this : I faced an issue when creating a the hashkeys with -1 (int) instead of boolean type. This code is more efficient when compared to using integer types.
+
 class MyHashSet {
 
 private:
