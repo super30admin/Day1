93bcebe21556fcc55b03d4da4827ee9cd0968482
Malav Soni <malav@live.com>
2024-10-23 00:43:43 +0530

    HashSet and MinStack Problem solved

---
+++ hash_set.js
@@ -0,0 +1,75 @@
+// Time Complexity : O(1)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : Yes, while creating an initial storage bucket array
+
+// Your code here along with comments explaining your approach
+
+var MyHashSet = function () {
+  this.primaryBucket = 1000;
+  this.secondaryBucket = 1000;
+  this.storage = this.createStorage(this.primaryBucket, this.secondaryBucket);
+};
+
+MyHashSet.prototype.createStorage = function (rows, columns) {
+  let array = new Array(rows);
+
+  for (let i = 0; i < rows; i++) {
+    array[i] = new Array(columns).fill(false); // Fill each inner array with 'false'
+  }
+
+  return array;
+};
+
+MyHashSet.prototype.getPrimaryIndex = function (key) {
+  return key % this.storage.length;
+};
+
+MyHashSet.prototype.getSecondaryIndex = function (key) {
+  return key / this.storage.length;
+};
+
+/**
+ * @param {number} key
+ * @return {void}
+ */
+MyHashSet.prototype.add = function (key) {
+  let primaryIndex = this.getPrimaryIndex(key);
+  if (this.storage[primaryIndex] == null) {
+    this.storage[primaryIndex] = [];
+  }
+  let secondaryIndex = this.getSecondaryIndex(key);
+  this.storage[primaryIndex][secondaryIndex] = true;
+};
+
+/**
+ * @param {number} key
+ * @return {void}
+ */
+MyHashSet.prototype.remove = function (key) {
+  let primaryIndex = this.getPrimaryIndex(key);
+  if (this.storage[primaryIndex] == null) {
+    return;
+  }
+  let secondaryIndex = this.getSecondaryIndex(key);
+  this.storage[primaryIndex][secondaryIndex] = false;
+};
+
+/**
+ * @param {number} key
+ * @return {boolean}
+ */
+MyHashSet.prototype.contains = function (key) {
+  let primaryIndex = this.getPrimaryIndex(key);
+  if (this.storage[primaryIndex] == null) {
+    return false;
+  }
+  let secondaryIndex = this.getSecondaryIndex(key);
+  return this.storage[primaryIndex][secondaryIndex] || false;
+};
+
+var obj = new MyHashSet();
+obj.add(1001);
+obj.add(1006);
+console.log("Contains Key" + obj.contains(1));
+console.log("Contains Key" + obj.contains(3));
---
+++ min_stack.js
@@ -0,0 +1,58 @@
+// Time Complexity : O(1)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach
+ 
+var MinStack = function () {
+  this.stack = [];
+  this.min = Infinity;
+  this.stack.insert(this.min);
+};
+
+/**
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function (val) {
+  if (val <= this.min) {
+    this.stack.push(this.min);
+    this.min = val;
+  }
+  this.stack.push(val);
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function () {
+  let poped = this.stack.pop();
+  if (poped == this.min) {
+    this.min = this.stack.pop();
+  }
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function () {
+  return this.stack[this.stack.length - 1];
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function () {
+  return this.min;
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
