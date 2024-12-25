51b45e058deeae0cfb82208071bfb2fdfd6c1a4d
Adwait Relekar <adrelekar23@gmail.com>
2024-12-25 17:57:23 -0500

    Problem 1 Updated

--- Problem_1.java
+++ Problem_1.java
@@ -2,26 +2,60 @@
 // Space Complexity : O(n)
 // Did this code successfully run on Leetcode : Yes
 // Any problem you faced while coding this : No
-// Approach: Used boolean array to track the keys directly
+// Approach: Used boolean 2D array and double hashing to track the keys
+// The primary hash determines the bucket, and the secondary hash determines the position within the bucket.
 
 class MyHashSet {
 
-    private boolean[] arr;
+    int buckets;
+    int bucketItems;
+    boolean[][] storage;
 
     public MyHashSet() {
-        this.arr = new boolean[1000001];
+        this.buckets = 1000;
+        this.bucketItems = 1000;
+        this.storage = new boolean[this.buckets][];
     }
 
+    public int getBucket(int key) {
+        return key % this.buckets;
+    }
+
+    public int getBucketItem(int key) {
+        return key / this.bucketItems;
+    }
+
+
     public void add(int key) {
-        arr[key] = true;
+        int bucket = getBucket(key);
+        int bucketItem = getBucketItem(key);
+        if(storage[bucket] == null) {
+            if(bucket == 0) {
+                storage[bucket] = new boolean[this.bucketItems+1];
+            } else {
+                storage[bucket] = new boolean[this.bucketItems];
+            }
+        }
+
+        storage[bucket][bucketItem] = true;
     }
 
     public void remove(int key) {
-        arr[key] = false;
+        int bucket = getBucket(key);
+        int bucketItem = getBucketItem(key);
+        if(storage[bucket] == null) {
+            return;
+        }
+        storage[bucket][bucketItem] = false;
     }
 
     public boolean contains(int key) {
-        return arr[key];
+        int bucket = getBucket(key);
+        int bucketItem = getBucketItem(key);
+        if(storage[bucket] == null) {
+            return false;
+        }
+        return storage[bucket][bucketItem];
     }
 }
 
