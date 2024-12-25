817ee32622663115b5e5f92e205b7a299409b5c4
Himani.Vaidya <85447801+hvaidya14@users.noreply.github.com>
2024-12-25 00:24:45 -0800

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -55,3 +55,59 @@ class MinStack {
         return min;
     }
 }
+
+
+//Time Complexity:0(1)
+//Space Complexity:Constant space to store the range of 2d arrays indexes and position in 2d arrays while addition, contains and removal.
+
+class MyHashSet {
+    int primaryIndexes=1000;
+    int secondaryIndexes=1000;
+    boolean hashArray[][];
+    public MyHashSet() {
+        hashArray = new boolean[primaryIndexes][];
+    }
+    
+
+    public int getHash1(int key) {
+        return key%1000;
+    }
+
+    public int getHash2(int key) {
+        return key/1000;
+    }
+
+    public void add(int key) {
+        int pointer1 = getHash1(key);
+        int pointer2 = getHash2(key);
+        if(hashArray[pointer1] == null) {
+            if (key == 1000000) {
+                hashArray[pointer1] = new boolean[secondaryIndexes+1];
+            } else {
+                hashArray[pointer1] = new boolean[secondaryIndexes];
+            }
+
+            hashArray[pointer1][pointer2] = true;
+        } else {
+            hashArray[pointer1][pointer2] = true;
+        }
+    }
+    
+    public void remove(int key) {
+        int pointer1 = getHash1(key);
+        int pointer2 = getHash2(key);
+        if (hashArray[pointer1] != null) {
+            hashArray[pointer1][pointer2] = false;
+        }
+
+    }
+    
+    public boolean contains(int key) {
+        int pointer1 = getHash1(key);
+        int pointer2 = getHash2(key);
+        if (hashArray[pointer1] == null) {
+            return false;
+        }
+        return  hashArray[pointer1][pointer2];
+    }
+}
