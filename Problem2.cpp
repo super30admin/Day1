47b22e8862cb4c026e9eaf4c95ba5e6bccc12b18
subhed <subhed561@gmail.com>
2024-12-24 20:26:43 -0700

    Adding code for Design-1 Problem

---
+++ Problem2.cpp
@@ -0,0 +1,46 @@
+class MinStack
+{
+
+private:
+    stack<int> s;
+    int min = INT_MAX;
+
+public:
+    MinStack()
+    {
+    }
+
+    void push(int val)
+    {
+
+        if (val <= min)
+        {
+            s.push(min);
+            min = val;
+        }
+
+        s.push(val);
+    }
+
+    void pop()
+    {
+
+        if (s.top() == min)
+        {
+            s.pop();
+            min = s.top();
+        }
+
+        s.pop();
+    }
+
+    int top()
+    {
+        return s.top();
+    }
+
+    int getMin()
+    {
+        return min;
+    }
+};
