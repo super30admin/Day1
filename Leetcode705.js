1e0150972fdc60608af1452cd62fa3b57f1f841c
Harsh <evolutionharsh@users.noreply.github.com>
2024-12-26 15:17:16 -0500

    Leetcode705.js

--- Leetcode705.js
+++ Leetcode705.js
@@ -8,8 +8,6 @@
 
 I assumed simple array with name as arr in javascript.The size of array is intialized to be 1000 inside constructor(). Since we have to add(),remove() and check in contains() inside this array using key approach so i used indexOf() function which normally checks index of value to verify if it's present in array or not on basis of which we pushed element in array if it's not existed previously.Otherwise if it's exists and same element adds up again then we checked index and if index found is greater equal to 0 then in remove() using delete we remove that value from array.The similar methodology utilized to check and return value as true and false using indexOf approach inside contains().
 
-
-var arr=[];
 var MyHashSet = function() {
    
     this.arr= [10000];
@@ -31,8 +29,8 @@ MyHashSet.prototype.add = function(key) {
  * @return {void}
  */
 MyHashSet.prototype.remove = function(key) {
-    const val = this.arr.indexOf(key);
-    if(val>=0)
+    var val = this.arr.indexOf(key);
+    if(val>0)
     {
          delete this.arr[val];
     }
