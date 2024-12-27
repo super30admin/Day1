e9632410295abb2b720974b1c9e9c640b61fb0ef
padmaanaokar <anaokar.p@northeastern.edu>
2024-12-27 02:00:29 -0500

    Pushing 2 files with TC and SC

--- 155_MinStack.java
+++ 155_MinStack.java
@@ -1,5 +1,5 @@
 // Time Complexity :O(1)
-// Space Complexity :O(n)^2
+// Space Complexity :O(n), using 1 stack
 // Did this code successfully run on Leetcode : Yes
 // Any problem you faced while coding this :NO
 
@@ -8,42 +8,76 @@
 
 //2nd Approach using 2 stacks
 class MinStack {
-    Stack<Integer> st;
-    Stack<Integer> minSt; //for first approach
+//1:1 mapping, using 2 stacks
+    /*Stack<Integer> mainSt;
+    Stack<Integer> minSt;
     int min;
     public MinStack() {
-        this.st = new Stack<Integer>();
-        this.minSt = new Stack<Integer>();
+        this.mainSt = new Stack<>();
+        this.minSt = new Stack<>();
+        minSt.push(Integer.MAX_VALUE);
         this.min = Integer.MAX_VALUE;
-        minSt.push(min);
-
     }
-
+    
     public void push(int val) {
-        this.min = Math.min(min, val); // First approach
-        minSt.push(min);
-        st.push(val);
+       this.min  = Math.min(min,val);
+        this.mainSt.push(val);
+        this.minSt.push(min);
 
 
     }
-
+    //In pop , pop from both
     public void pop() {
-        if(st.peek() != null){
-            st.pop();
-            minSt.pop(); //after poping , min will be whatever is at the top of the minstack, done below
-            min = minSt.peek();// v imp : you have to re assign again ;
+        mainSt.pop();
+        minSt.pop();
+        this.min  = minSt.peek();
+    }
+    
+    public int top() {
+        return mainSt.peek();
+    }
+    
+    public int getMin() {
+        return this.min;// minSt.peek();
+    }*/
+
+
+    //Using 1 stack, here whenever a curr min changes, we add the previous min under it. Also, when we pop, if its equal to curr min, pop the previous min too
+    Stack<Integer> mainSt;
+    int min;
+    public MinStack() {
+        this.mainSt = new Stack<>();
+        this.min = Integer.MAX_VALUE;
+    }
+    //O(1)
+    public void push(int val) {
+        if(val <= this.min){
+            mainSt.push(min);
+            min = val;
+
+        }
+        mainSt.push(val);
+
+
+    }
+    //O(1)
+    //In pop , if pop = currmin, 
+    public void pop() {
+        if(mainSt.pop() == min){// check if popped elemnt is the curr min
+            min = mainSt.pop();// if popped is currmin , then assign prev min as curr min
         }
 
     }
-
+    //O(1)
     public int top() {
-        return st.peek();
+        return mainSt.peek();
     }
-
+    //O(1)
     public int getMin() {
-        return  minSt.peek();
+        return this.min;// minSt.peek();
     }
 }
+
 /**
  * Your MinStack object will be instantiated and called as such:
  * MinStack obj = new MinStack();
@@ -52,37 +86,3 @@ class MinStack {
  * int param_3 = obj.top();
  * int param_4 = obj.getMin();
  */
-
-//Using just 1 stack
-class MinStack {
-    Stack<Integer> st;
-    int min;
-
-    public MinStack() {
-        this.st = new Stack<Integer>();
-        this.min = Integer.MAX_VALUE;
-    }
-
-    public void push(int val) {
-        if(val <= min){
-            st.push(min);
-            min=val;
-        }
-        st.push(val);
-    }
-
-    public void pop() {
-// check if element coming out is current min : below
-        if(st.pop()== min){ // if 4 , 4: then again we will be storing 4 ke neeche 4 and assigning old 4 to min
-            min = st.pop(); // set your min to prev min
-        }
-    }
-
-    public int top() {
-        return st.peek();
-    }
-
-    public int getMin() {
-        return min; //this works for other as well
-    }
-}
