6c55785d800cdd763d5336fe56edc91d9975b542
SukeshSrinivas <123338848+SukeshSrinivas@users.noreply.github.com>
2025-01-09 15:24:44 -0600

    Done

--- Design hashSet.py
+++ Design hashSet.py
@@ -17,3 +17,19 @@ class MyHashSet(object):
 
     def contains(self, key):
         return key in self.hashSet  # Check if the key exists in the dictionary
+    
+
+    #Add hashset 
+    #here I used dictionary for simplicity. 
+    #It only adds the non duplicate entries
+    
+    #Remove element
+    #If the value is present then only remove from the hashset
+
+    #Contains
+    #Here it will check boolean if the element is present
+
+    #Time complexity
+    #Here it is only O(1) 
+
+    #Space complexity is also O(1)
