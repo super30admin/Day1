b99cc8c2240e6d337228872be77fc91f30793110
bhavana4992 <32822224+bhavana4992@users.noreply.github.com>
2025-01-22 12:48:44 -0600

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,7 +1,84 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity : add, remove and contains-> O(1)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes 
+// Any problem you faced while coding this : forgot to inclue edge case for 0 and later added it
 
 
-// Your code here along with comments explaining your approach
+class Sample {
+
+    private boolean[][] storage;
+    private int primary;
+    private int nested;
+
+    private int hashPrimary(int key){
+
+
+        return key%primary;
+    }
+
+  private int hashSecondary(int key){
+
+
+        return key/nested;
+    }
+
+    public MyHashSet() {   
+
+        this.primary=1000;
+        this.nested=1000;
+        this.storage=new boolean[primary][];
+
+    }
+    
+    public void add(int key) {
+
+        int bucket = hashPrimary(key);
+        if(storage[bucket] == null){
+            if(bucket==0){
+                storage[bucket]=new boolean[nested+1];
+            }
+              else{
+             storage[bucket]=new boolean[nested];
+
+                }
+
+
+        }
+
+        int bucketItem = hashSecondary(key);
+        storage[bucket][bucketItem]=true;
+        
+        
+    }
+    
+    public void remove(int key) {
+
+        int bucket = hashPrimary(key);
+        if(storage[bucket]==null){
+
+            return ;
+        }
+        else{
+
+            int bucketItem=hashSecondary(key);
+            storage[bucket][bucketItem]=false;
+        }
+
+        
+    }
+    
+    public boolean contains(int key) {
+                int bucket = hashPrimary(key);
+                  if(storage[bucket]==null){
+                     return  false;
+                   }
+        else{
+            int bucketItem=hashSecondary(key);
+            return storage[bucket][bucketItem];
+        }
+
+
+        
+    }
+}
+
