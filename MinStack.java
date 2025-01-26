e905e2ad33ad9907d25ee2d35f1775826100d047
uma lakkaraju <u0l006r@walmart.com>
2025-01-25 16:42:02 -0800

    LC-115-MinStack

--- MinStack.java
+++ MinStack.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class MinStack {
+import java.util.Stack;
+
+public class MinStack{
+    Stack<Integer> mainSt;
+    Stack<Integer> minSt;
+    int min;
+
+    public MinStack() {
+        mainSt=new Stack<>();
+        minSt=new Stack<>();
+        min=Integer.MAX_VALUE;
+        minSt.push(min);
+    }
+
+    public void push(int val) {
+        mainSt.push(val);
+        min=Math.min(min,val);
+        minSt.push(min);
+    }
+
+    public void pop() {
+        mainSt.pop();
+        minSt.pop();
+        min=minSt.peek();
+    }
+
+    public int top() {
+        return mainSt.peek();
+    }
+
+    public int getMin() {
+        return min;
+    }
 }
+
+
